85e5be5ed04889bd3a155653c51c2dcec0936ee6
Kyle McCracken <kmccracken150@gmail.com>
2022-08-29 12:30:13 -0400

    separate connect logic, create navbar, update layout structure and logic

---
+++ App/src/lib/navbar/Navbar.svelte
@@ -0,0 +1,40 @@
+<script>
+  import '../../app.css';
+  import { socketGlobal } from '../../stores';
+
+  const disconnect = () => {
+    $socketGlobal.close();
+    socketGlobal.update(() => null);
+  };
+</script>
+
+<nav>
+  {#if $socketGlobal}
+    <nav-left>
+      <a href="/">Listen 🎧</a>
+      <a href="/socketman">Socketman 🚀</a>
+    </nav-left>
+    <nav-right>
+      <button type="button" on:click={disconnect}>Disconnect</button>
+    </nav-right>
+  {/if}
+</nav>
+
+<style>
+  nav {
+    display: flex;
+    justify-content: space-between;
+    height: 50px;
+    background-color: rgb(64, 64, 64);
+    padding: 0 30px;
+  }
+  nav * {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  nav-left * {
+    text-align: center;
+    padding: 0 20px;
+  }
+</style>

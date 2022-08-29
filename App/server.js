240266ea576fa478f722b4bcaa723c92536835c4
Kyle McCracken <kmccracken150@gmail.com>
2022-08-29 12:02:58 -0400

    clear out server.js to prevent confusion

--- App/server.js
+++ App/server.js
@@ -1,18 +1,18 @@
-// server.js
-import http from 'http';
-import { handler } from './build/handler.js'; // <- Import SvelteKit handlers
-import socketHandler from './src/socketHandler.js'; // The SocketIO stuff (see next step)
-import express from 'express';
+// // server.js
+// import http from 'http';
+// import { handler } from './build/handler.js'; // <- Import SvelteKit handlers
+// import socketHandler from './src/socketHandler.js'; // The SocketIO stuff (see next step)
+// import express from 'express';
 
-const app = express();
-const server = http.createServer(app);
+// const app = express();
+// const server = http.createServer(app);
 
-// Inject SocketIO
-socketHandler(server);
+// // Inject SocketIO
+// socketHandler(server);
 
-// SvelteKit handlers
-app.use(handler);
+// // SvelteKit handlers
+// app.use(handler);
 
-server.listen(3000, () => {
-	console.log('Running on http://localhost:3000');
-});
+// server.listen(3000, () => {
+// 	console.log('Running on http://localhost:3000');
+// });

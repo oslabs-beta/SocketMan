a687b2669b6d20d4772b989cd74af85dd929335a
Kyle McCracken <kmccracken150@gmail.com>
2022-08-30 21:35:27 -0400

    work in progress contenteditable div highlight.js

--- App/src/lib/connect/Connect.svelte
+++ App/src/lib/connect/Connect.svelte
@@ -1,7 +1,112 @@
 <script>
   import ioClient from 'socket.io-client';
+  import { bind } from 'svelte/internal';
   import { socketGlobal } from '../../stores';
   import { allEventsGlobal } from '../../stores';
+
+  import hljs from 'highlight.js/lib/core';
+  import javascript from 'highlight.js/lib/languages/javascript';
+  hljs.registerLanguage('javascript', javascript);
+  // console.log(hljs);
+  // console.log(hljs.listLanguages());
+
+  class Cursor {
+    static getCurrentCursorPosition(parentElement) {
+      var selection = window.getSelection(),
+        charCount = -1,
+        node;
+
+      if (selection.focusNode) {
+        if (Cursor._isChildOf(selection.focusNode, parentElement)) {
+          node = selection.focusNode;
+          charCount = selection.focusOffset;
+
+          while (node) {
+            if (node === parentElement) {
+              break;
+            }
+
+            if (node.previousSibling) {
+              node = node.previousSibling;
+              charCount += node.textContent.length;
+            } else {
+              node = node.parentNode;
+              if (node === null) {
+                break;
+              }
+            }
+          }
+        }
+      }
+
+      return charCount;
+    }
+
+    static setCurrentCursorPosition(chars, element) {
+      if (chars >= 0) {
+        var selection = window.getSelection();
+
+        let range = Cursor._createRange(element, { count: chars });
+
+        if (range) {
+          range.collapse(false);
+          selection.removeAllRanges();
+          selection.addRange(range);
+        }
+      }
+    }
+
+    static _createRange(node, chars, range) {
+      if (!range) {
+        range = document.createRange();
+        range.selectNode(node);
+        range.setStart(node, 0);
+      }
+
+      if (chars.count === 0) {
+        range.setEnd(node, chars.count);
+      } else if (node && chars.count > 0) {
+        if (node.nodeType === Node.TEXT_NODE) {
+          if (node.textContent.length < chars.count) {
+            chars.count -= node.textContent.length;
+          } else {
+            range.setEnd(node, chars.count);
+            chars.count = 0;
+          }
+        } else {
+          for (var lp = 0; lp < node.childNodes.length; lp++) {
+            range = Cursor._createRange(node.childNodes[lp], chars, range);
+
+            if (chars.count === 0) {
+              break;
+            }
+          }
+        }
+      }
+
+      return range;
+    }
+
+    static _isChildOf(node, parentElement) {
+      while (node !== null) {
+        if (node === parentElement) {
+          return true;
+        }
+        node = node.parentNode;
+      }
+
+      return false;
+    }
+  }
+
+  const updateFormat = (e) => {
+    // reformat
+    const oldPos = Cursor.getCurrentCursorPosition(e.target);
+    hljs.highlightElement(e.target);
+    Cursor.setCurrentCursorPosition(oldPos, e.target);
+
+    // update state
+  };
 
   //used to capture value of user server URL
   let connectTo = '';
@@ -59,10 +164,20 @@
 <svelte:head>
   <title>GUI Welcome</title>
   <meta name="description" content="GUI welcome" />
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/dark.min.css"
+  />
 </svelte:head>
 
 <!-- CONNECT TO SERVER SECTION -->
 <section>
+  <div
+    class="texty language-javascript javascript"
+    spellcheck="false"
+    contenteditable="true"
+    on:input={updateFormat}
+  />
   <h1>GUI Interface</h1>
   <div class="connect-container">
     <input
@@ -77,6 +192,12 @@
 </section>
 
 <style>
+  .texty {
+    display: inline-block;
+    min-width: 50px;
+    width: 100%;
+    min-height: 50px;
+  }
   .connect-container {
     display: flex;
     flex-direction: column;

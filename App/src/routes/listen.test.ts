f81ab1071050c5dc3d8bd665bd8682af5526b030
nia-h <nia.hq@yahoo.com>
2022-10-26 11:01:14 -0400

    set up socket.io-mock-ts for testing

--- App/src/routes/listen.test.ts
+++ App/src/routes/listen.test.ts
@@ -3,9 +3,26 @@ import '@testing-library/jest-dom';
 import { render, fireEvent, screen } from '@testing-library/svelte';
 
 import Listen from './+page.svelte';
+import { SocketServerMock } from 'socket.io-mock-ts';
+//const Client = require('socket.io-client');
 
 test('shows proper heading when rendered', () => {
   render(Listen);
   const heading = screen.getByText('Events Log');
   expect(heading).toBeInTheDocument();
+});
+
+describe("Listen for events from user's server socket", () => {
+  const socket = new SocketServerMock();
+  const client = socket.clientMock;
+  test('renders received event from user server', (done) => {
+    //clientSocket represents our gui
+    client.on('hello', (arg: any) => {
+      expect(arg).toBe('world');
+      //done();
+    });
+
+    //mimics emit from user's server
+    socket.emit('hello', 'world');
+  });
 });
